0b20d09e51e868f81a072993841760a5c9bd185f
root <you@example.com>
2022-05-12 22:40:47 +0800

    completed all panels expect panel 1

---
+++ main.js
@@ -0,0 +1,11 @@
+$(".menu").hide();
+    $(".page-navbar-wrapper").hide();
+
+    function menu(){
+        $(".menu").show();
+        $(".page-navbar-wrapper").show();
+        $(".page-content").hide();
+        $(".home-button-page").hide();
+
+
+    }
